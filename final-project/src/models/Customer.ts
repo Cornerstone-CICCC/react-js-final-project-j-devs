36aaf4d31cfa09458d196e914564043a033a6118
Jose Muzquiz <jamuzquizgarcia@gmail.com>
2025-06-08 01:06:41 -0700

    customer/product actions || api route for getAll and add customers

---
+++ final-project/src/models/Customer.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const CustomerSchema  = new mongoose.Schema({
+  firstname: { type: String, require: true },
+  lastname: { type: String, require: true },
+  age: { type: Number, require: true },
+  email: { type: String, require: true },
+  password: { type: String, require: true }
+})
+
+export default mongoose.models.Customer || mongoose.model("Customer", CustomerSchema)
